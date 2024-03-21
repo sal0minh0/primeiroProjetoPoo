ac3c3cc085b9117fdab8f8f1de202d14a85098e4
Salominho <salomaomoraes1@hotmail.com>
2024-03-20 22:03:25 -0300

    incluidos mais 2 métodos e finalizando o projetogit add . :)

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 >
 > Nome: Salomão de Moraes Cassiano Barbosa
 >
+> Tema: Sistema de Controle de Estoque
+>
 > O que foi feito - Requisitos Mínimos:
 > 
-> (...)
+> - Métodos com vetorees para adicionar, remover, consultar, atualizar os produtos (e tratados situações como a remoção de produtos que não estão no estoque)
 >
 > O que foi feito - Requisitos Adicionais:
 >
-> (...)
+> - Uso de variáveis estáticas
 >
-> .
+
--- src/projeto.java
+++ src/projeto.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
 public class projeto {
-    static Scanner sc = new Scanner(System.in);
 
+        //declarar os variáveis estaticas
+        static Scanner sc = new Scanner(System.in);
         static int maximo; //maximo do estoque
         static String[] nome; //o produto
         static int[] quantidade; //quantidade de produtos
@@ -11,12 +12,12 @@ public class projeto {
         static int indice = 0; //tamanho do vetor
 
 
-    //adiciona Produtos
+    //adicionar Produtos
 
     public static void adicionarProduto() {
 
-
         if (indice < maximo) {
+
             System.out.println("Qual o nome desse produto? ");
             nome[indice] = sc.nextLine();
             System.out.println("Quantos desse produto você deseja adicionar? ");
@@ -28,8 +29,43 @@ public class projeto {
             System.out.println("Qual a validade do produto? ");
             validade[indice] = sc.nextLine();
             indice++;
+
         } else {
+            System.out.println();
             System.out.println("Estoque está cheio.");
+            System.out.println();
+        }
+    }
+
+    //remoção de Produtos
+
+    public static void removerProduto() {
+        System.out.println("Qual o nome do produto que você deseja remover? ");
+        String produtoParaRemover = sc.nextLine();
+
+        boolean acharProduto = false;
+        for (int i = 0; i < indice; i++) {
+            if (nome[i].equals(produtoParaRemover)) {
+
+                for (int j = i; j < indice - 1; j++) {
+                    nome[j] = nome[j + 1];
+                    quantidade[j] = quantidade[j + 1];
+                    preco[j] = preco[j + 1];
+                    validade[j] = validade[j + 1];
+                }
+                indice--;
+                System.out.println();
+                System.out.println("Produto removido com sucesso.");
+                System.out.println();
+                acharProduto = true;
+                break;
+            }
+        }
+
+        if (acharProduto == false) {
+            System.out.println();
+            System.out.println("Produto não encontrado.");
+            System.out.println();
         }
     }
 
@@ -40,23 +76,60 @@ public class projeto {
             System.out.println();
             System.out.println("Sem Produto no estoque, Use a primeira opção para registrar seu produto.");
             System.out.println();
+
         } else {
 
         for (int i = 0; i < indice; i++) {
             System.out.println();
             System.out.println("Produto " + (i + 1) + ":");
             System.out.println("Nome: " + nome[i]);
-            System.out.println("Quantidade: " + quantidade[i]);
-            System.out.println("Preço: " + preco[i]);
+            System.out.println("Quantidade: " + quantidade[i] + " " + nome[i] + "(s)");
+            System.out.printf("Preço: R$%.2f\n", preco[i]);
             System.out.println("Validade: " + validade[i]);
             System.out.println();
+
             }
         }
     }
 
+    //atualizar o produto selecionado
+
+    public static void atualizarProduto() {
+        System.out.println("Você deseja atualizar qual produto? ");
+        String atual = sc.nextLine();
+
+        for (int i = 0; i < indice; i++) {
+
+            if (nome[i].equals(atual)) {
+
+                System.out.println("Trocando as informações...");
+                System.out.println("Digite o nome correto do produto: ");
+                nome[i] = sc.nextLine();
+                System.out.println("Digite o número correto de produtos você deseja adicionar: ");
+                quantidade[i] = sc.nextInt();
+                sc.nextLine();
+                System.out.println("Qual o preço correto do produto?");
+                preco[i] = sc.nextDouble();
+                sc.nextLine();
+                System.out.println("Qual a validade correta do produto?");
+                validade[i] = sc.nextLine();
+                return;
+            }
+            if (!nome[i].equals(atual)) {
+                System.out.println();
+                System.out.println("Produto não encontrado.");
+                System.out.println();
+                break;
+            }
+        }
+
+    }
+
+
     public static void main(String[] args) {
 
         //interface para iniciar
+
         System.out.println("Digite 'i' para iniciar e outro digito para sair.");
         char c = sc.next().charAt(0);
         sc.nextLine();
@@ -81,8 +154,8 @@ public class projeto {
 
             System.out.println("Menu interativo - Controle de Estoque (escolha uma opção): ");
             System.out.println("1 - Adicionar Produto");
-            System.out.println("2 - Consultar Produto");
-            System.out.println("3 - Remover Produto");
+            System.out.println("2 - Remover Produto");
+            System.out.println("3 - Consultar Produtos");
             System.out.println("4 - Atualizar Produto");
             System.out.println("5 - Sair");
             int opcao = Integer.parseInt(sc.nextLine());
@@ -91,18 +164,34 @@ public class projeto {
 
             switch (opcao) {
                 case 1:
+
                     adicionarProduto();
                     break;
-                    
+
                 case 2:
+
+                    removerProduto();
+                    break;
+
+                case 3:
+
                     consultarProdutos();
+                    break;
+
+                case 4:
+                    atualizarProduto();
+                    break;
+
+                case 5:
+
+                    c = 's';
                     break;
 
                 default:
                     System.out.println("Opção inválida.");
                     break;
             }
-            break;
+
 
         }
 
